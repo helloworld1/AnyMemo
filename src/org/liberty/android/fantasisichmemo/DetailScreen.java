a56d08050dab26c9dd8a1f79e7c88448a4b8a0d7
Liberty <mrlhwliberty@gmail.com>
2010-01-18 09:48:45 -0500

    Add the html file to git

---
+++ src/org/liberty/android/fantasisichmemo/DetailScreen.java
@@ -0,0 +1,15 @@
+package org.liberty.android.fantasisichmemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DetailScreen extends Activity {
+	
+	
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail_screen);	
+    }
+	
+
+}
