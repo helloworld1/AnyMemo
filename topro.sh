d88824ecb66364d172000b0ac1bd72d1731ff13a
Liberty <mrlhwliberty@gmail.com>
2010-04-27 22:15:31 -0400

    modify documents

--- topro.sh
+++ topro.sh
@@ -12,8 +12,8 @@ mv src/org/liberty/android/fantastischmemo src/org/liberty/android/fantastischme
 find ./ -type f -name "*.java" | xargs sed -i 's/org\.liberty\.android\.fantastischmemo/org\.liberty\.android\.fantastischmemopro/g'
 find ./ -type f -name "*.xml" | xargs sed -i 's/org\.liberty\.android\.fantastischmemo/org\.liberty\.android\.fantastischmemopro/g'
 sed -i 's/Free Edition/Pro Edition/g' ./assets/about.html
-sed -i 's/Fantastisch Memo\\nFree/Fantastisch Memo/g' ./res/values/strings.xml
-sed -i 's/\"app_name\">Fantastisch Memo/\"app_name\">Fantastisch Memo Pro/g' ./res/values/strings.xml
+sed -i 's/AnyMemo Free/AnyMemo/g' ./res/values/strings.xml
+sed -i 's/\"app_name\">AnyMemo/\"app_name\">AnyMemo Pro/g' ./res/values/strings.xml
 
 
 
