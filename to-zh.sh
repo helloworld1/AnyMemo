ad9933ee7a11da5a55e0960460bc4d0ab119b65d
Liberty <mrlhwliberty@gmail.com>
2010-05-24 08:18:35 -0400

    add missing png files

--- to-zh.sh
+++ to-zh.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-
-rm -rf res/values-zh-rTW/strings.xml
+if [ -d "res/values-zh-rTW" ]
+then
+        rm -rf res/values-zh-rTW
+fi
+mkdir res/values-zh-rTW
 iconv -f utf8 -t gb2312 res/values-zh-rCN/strings.xml | iconv -f gb2312 -t big5 | iconv -f big5 -t utf8 > res/values-zh-rTW/strings.xml 
 
 
