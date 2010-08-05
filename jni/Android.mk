a67b9b6014b5a66f5d34e3a2538d3fc379f1e1e3
Liberty <mrlhwliberty@gmail.com>
2010-08-05 17:40:31 -0400

    remove unncecssary files

--- jni/Android.mk
+++ jni/Android.mk
@@ -3,6 +3,6 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := sqlite3
-LOCAL_SRC_FILES := shell.c  sqlite3.c
+LOCAL_SRC_FILES := sqlite3.c
 
 include $(BUILD_SHARED_LIBRARY)
