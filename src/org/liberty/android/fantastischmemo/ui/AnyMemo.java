223a660e64770b313e4df4b3e8ca1a45a0459987
Howard Liberty <liberty@anymemo.org>
2012-01-12 22:02:47 -0800

    AnyMemo main screen can display tabs.

--- src/org/liberty/android/fantastischmemo/ui/AnyMemo.java
+++ src/org/liberty/android/fantastischmemo/ui/AnyMemo.java
@@ -19,20 +19,151 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 package org.liberty.android.fantastischmemo.ui;
 
+import java.util.HashMap;
+
 import org.liberty.android.fantastischmemo.AMActivity;
+import org.liberty.android.fantastischmemo.AMActivity;
+import org.liberty.android.fantastischmemo.R;
 
 import org.liberty.android.fantastischmemo.ui.CardEditor;
 
+import android.content.Context;
 import android.content.Intent;
+
+import android.content.res.Resources;
 import android.os.Bundle;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+
+import android.view.View;
+
+import android.widget.TabHost;
+
 public class AnyMemo extends AMActivity {
+    TabHost mTabHost;
+    TabManager mTabManager;
+
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent myIntent = new Intent(this, CardEditor.class);
+
+        setContentView(R.layout.main_tabs);
+        mTabHost = (TabHost)findViewById(android.R.id.tabhost);
+        mTabHost.setup();
+
+        Resources res = getResources();
+        mTabManager = new TabManager(this, mTabHost, android.R.id.tabcontent);
+
+        mTabManager.addTab(mTabHost.newTabSpec("misc").setIndicator(getString(R.string.misc_tab_text),  res.getDrawable(R.drawable.misc)),
+                MiscTabFragment.class, null);
+
+        if (savedInstanceState != null) {
+            mTabHost.setCurrentTabByTag(savedInstanceState.getString("misc"));
+        }
+        //Intent myIntent = new Intent(this, CardEditor.class);
         //myIntent.putExtra("dbpath", "/sdcard/french-body-parts.db");
-        myIntent.putExtra("dbpath", "/sdcard/gre01.db");
-        myIntent.putExtra("id", 1);
-        startActivity(myIntent);
+        //myIntent.putExtra("dbpath", "/sdcard/gre01.db");
+        //myIntent.putExtra("id", 1);
+        //startActivity(myIntent);
     }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("tab", mTabHost.getCurrentTabTag());
+    }
+
+    public static class TabManager implements TabHost.OnTabChangeListener {
+        private final FragmentActivity mActivity;
+        private final TabHost mTabHost;
+        private final int mContainerId;
+        private final HashMap<String, TabInfo> mTabs = new HashMap<String, TabInfo>();
+        TabInfo mLastTab;
+
+        static final class TabInfo {
+            private final String tag;
+            private final Class<?> clss;
+            private final Bundle args;
+            private Fragment fragment;
+
+            TabInfo(String _tag, Class<?> _class, Bundle _args) {
+                tag = _tag;
+                clss = _class;
+                args = _args;
+            }
+        }
+
+        static class DummyTabFactory implements TabHost.TabContentFactory {
+            private final Context mContext;
+
+            public DummyTabFactory(Context context) {
+                mContext = context;
+            }
+
+            @Override
+            public View createTabContent(String tag) {
+                View v = new View(mContext);
+                v.setMinimumWidth(0);
+                v.setMinimumHeight(0);
+                return v;
+            }
+        }
+
+        public TabManager(FragmentActivity activity, TabHost tabHost, int containerId) {
+            mActivity = activity;
+            mTabHost = tabHost;
+            mContainerId = containerId;
+            mTabHost.setOnTabChangedListener(this);
+        }
+
+        public void addTab(TabHost.TabSpec tabSpec, Class<?> clss, Bundle args) {
+            tabSpec.setContent(new DummyTabFactory(mActivity));
+            String tag = tabSpec.getTag();
+
+            TabInfo info = new TabInfo(tag, clss, args);
+
+            // Check to see if we already have a fragment for this tab, probably
+            // from a previously saved state.  If so, deactivate it, because our
+            // initial state is that a tab isn't shown.
+            info.fragment = mActivity.getSupportFragmentManager().findFragmentByTag(tag);
+            if (info.fragment != null && !info.fragment.isDetached()) {
+                FragmentTransaction ft = mActivity.getSupportFragmentManager().beginTransaction();
+                ft.detach(info.fragment);
+                ft.commit();
+            }
+
+            mTabs.put(tag, info);
+            mTabHost.addTab(tabSpec);
+        }
+
+        @Override
+        public void onTabChanged(String tabId) {
+            TabInfo newTab = mTabs.get(tabId);
+            if (mLastTab != newTab) {
+                FragmentTransaction ft = mActivity.getSupportFragmentManager().beginTransaction();
+                if (mLastTab != null) {
+                    if (mLastTab.fragment != null) {
+                        ft.detach(mLastTab.fragment);
+                    }
+                }
+                if (newTab != null) {
+                    if (newTab.fragment == null) {
+                        newTab.fragment = Fragment.instantiate(mActivity,
+                                newTab.clss.getName(), newTab.args);
+                        ft.add(mContainerId, newTab.fragment, newTab.tag);
+                    } else {
+                        ft.attach(newTab.fragment);
+                    }
+                }
+
+                mLastTab = newTab;
+                ft.commit();
+                mActivity.getSupportFragmentManager().executePendingTransactions();
+            }
+        }
+    }
+
+
 }
---
+++ src/org/liberty/android/fantastischmemo/ui/MiscTabFragment.java
@@ -0,0 +1,299 @@
+/*
+Copyright (C) 2010 Haowen Ning
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
+package org.liberty.android.fantastischmemo.ui;
+
+import org.liberty.android.fantastischmemo.OptionScreen;
+import org.liberty.android.fantastischmemo.R;
+
+import org.liberty.android.fantastischmemo.converter.CSVExporter;
+import org.liberty.android.fantastischmemo.converter.CSVImporter;
+import org.liberty.android.fantastischmemo.converter.ConvertScreen;
+import org.liberty.android.fantastischmemo.converter.MnemosyneXMLExporter;
+import org.liberty.android.fantastischmemo.converter.MnemosyneXMLImporter;
+import org.liberty.android.fantastischmemo.converter.QATxtExporter;
+import org.liberty.android.fantastischmemo.converter.QATxtImporter;
+import org.liberty.android.fantastischmemo.converter.Supermemo2008XMLImporter;
+import org.liberty.android.fantastischmemo.converter.SupermemoXMLImporter;
+import org.liberty.android.fantastischmemo.converter.TabTxtExporter;
+import org.liberty.android.fantastischmemo.converter.TabTxtImporter;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+
+import android.net.Uri;
+import android.os.Bundle;
+
+import android.preference.PreferenceManager;
+
+import android.support.v4.app.Fragment;
+
+import android.text.Html;
+
+import android.text.method.LinkMovementMethod;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.TextView;
+
+/* 
+ * This class is invoked when the user share the card from other
+ * apps like ColorDict 
+ */
+public class MiscTabFragment extends Fragment implements View.OnClickListener{
+    private static final String WEBSITE_VERSION="http://anymemo.org/index.php?page=version";
+    private Activity mActivity;
+    private View optionButton;
+    private View importButton;
+    private View exportButton;
+    private View importItems;
+    private View exportItems;
+    private View importMnemosyneButton;
+    private View importSupermemoButton;
+    private View importCSVButton;
+    private View importTabButton;
+    private View importQAButton;
+    private View importSupermemo2008Button;
+    private View exportMnemosyneButton;
+    private View exportCSVButton;
+    private View exportTabButton;
+    private View exportQAButton;
+
+    private View mergeButton;
+    private View resetButton;
+    private View donateButton;
+    private View helpButton;
+    private View aboutButton;
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        mActivity = activity;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.misc_tab, container, false);
+        optionButton = v.findViewById(R.id.misc_options);
+        optionButton.setOnClickListener(this);
+        importButton = v.findViewById(R.id.misc_import);
+        importButton.setOnClickListener(this);
+        exportButton = v.findViewById(R.id.misc_export);
+        exportButton.setOnClickListener(this);
+        importItems = v.findViewById(R.id.import_items);
+        exportItems = v.findViewById(R.id.export_items);
+        importMnemosyneButton = v.findViewById(R.id.import_mnemosyne);
+        importMnemosyneButton.setOnClickListener(this);
+        importSupermemoButton = v.findViewById(R.id.import_supermemo);
+        importSupermemoButton.setOnClickListener(this);
+        importCSVButton = v.findViewById(R.id.import_csv);
+        importCSVButton.setOnClickListener(this);
+        importTabButton = v.findViewById(R.id.import_tab);
+        importTabButton.setOnClickListener(this);
+        importQAButton = v.findViewById(R.id.import_qa);
+        importQAButton.setOnClickListener(this);
+        importSupermemo2008Button = v.findViewById(R.id.import_supermemo_2008);
+        importSupermemo2008Button.setOnClickListener(this);
+        exportMnemosyneButton = v.findViewById(R.id.export_mnemosyne);
+        exportMnemosyneButton.setOnClickListener(this);
+        exportCSVButton = v.findViewById(R.id.export_csv);
+        exportCSVButton.setOnClickListener(this);
+        exportTabButton = v.findViewById(R.id.export_tab);
+        exportTabButton.setOnClickListener(this);
+        exportQAButton = v.findViewById(R.id.export_qa);
+        exportQAButton.setOnClickListener(this);
+
+        mergeButton = v.findViewById(R.id.misc_merge);
+        mergeButton.setOnClickListener(this);
+        resetButton = v.findViewById(R.id.misc_reset);
+        resetButton.setOnClickListener(this);
+
+        donateButton = v.findViewById(R.id.misc_donate);
+        donateButton.setOnClickListener(this);
+        helpButton = v.findViewById(R.id.misc_help);
+        helpButton.setOnClickListener(this);
+        aboutButton = v.findViewById(R.id.misc_about);
+        aboutButton.setOnClickListener(this);
+        return v;
+    }
+
+    @Override
+    public void onClick(View v){
+        if(v == optionButton){
+            startActivity(new Intent(mActivity, OptionScreen.class));
+        }
+        /* Toggle visibility for import and export buttons */
+        if(v == importButton){
+            if(importItems.getVisibility() == View.GONE){
+                importItems.setVisibility(View.VISIBLE);
+            }
+            else{
+                importItems.setVisibility(View.GONE);
+            }
+        }
+
+        if(v == exportButton){
+            if(exportItems.getVisibility() == View.GONE){
+                exportItems.setVisibility(View.VISIBLE);
+            }
+            else{
+                exportItems.setVisibility(View.GONE);
+            }
+        }
+
+        if(v == importMnemosyneButton){
+            Intent myIntent = new Intent(mActivity, ConvertScreen.class);
+            myIntent.putExtra("file_extension", ".xml");
+            myIntent.putExtra("converter", MnemosyneXMLImporter.class);
+            startActivity(myIntent);
+        }
+        if(v == importSupermemoButton){
+            Intent myIntent = new Intent(mActivity, ConvertScreen.class);
+            myIntent.putExtra("file_extension", ".xml");
+            myIntent.putExtra("converter", SupermemoXMLImporter.class);
+            startActivity(myIntent);
+        }
+        if(v == importCSVButton){
+            Intent myIntent = new Intent(mActivity, ConvertScreen.class);
+            myIntent.putExtra("file_extension", ".csv");
+            myIntent.putExtra("converter", CSVImporter.class);
+            startActivity(myIntent);
+        }
+        if(v == importTabButton){
+            Intent myIntent = new Intent(mActivity, ConvertScreen.class);
+            myIntent.putExtra("file_extension", ".txt");
+            myIntent.putExtra("converter", TabTxtImporter.class);
+            startActivity(myIntent);
+        }
+        if(v == importQAButton){
+            Intent myIntent = new Intent(mActivity, ConvertScreen.class);
+            myIntent.putExtra("file_extension", ".txt");
+            myIntent.putExtra("converter", QATxtImporter.class);
+            startActivity(myIntent);
+        }
+        if(v == importSupermemo2008Button) {
+            Intent myIntent = new Intent(mActivity, ConvertScreen.class);
+            myIntent.putExtra("file_extension", ".xml");
+            myIntent.putExtra("converter", Supermemo2008XMLImporter.class);
+            startActivity(myIntent);
+        }
+        if(v == exportMnemosyneButton){
+            Intent myIntent = new Intent(mActivity, ConvertScreen.class);
+            myIntent.putExtra("file_extension", ".db");
+            myIntent.putExtra("converter", MnemosyneXMLExporter.class);
+            startActivity(myIntent);
+        }
+        if(v == exportCSVButton){
+            Intent myIntent = new Intent(mActivity, ConvertScreen.class);
+            myIntent.putExtra("file_extension", ".db");
+            myIntent.putExtra("converter", CSVExporter.class);
+            startActivity(myIntent);
+        }
+        if(v == exportTabButton){
+            Intent myIntent = new Intent(mActivity, ConvertScreen.class);
+            myIntent.putExtra("file_extension", ".db");
+            myIntent.putExtra("converter", TabTxtExporter.class);
+            startActivity(myIntent);
+        }
+        if(v == exportQAButton){
+            Intent myIntent = new Intent(mActivity, ConvertScreen.class);
+            myIntent.putExtra("file_extension", ".db");
+            myIntent.putExtra("converter", QATxtExporter.class);
+            startActivity(myIntent);
+        }
+        if(v == mergeButton){
+            Intent myIntent = new Intent(mActivity, DatabaseMerger.class);
+            startActivity(myIntent);
+        }
+        if(v == resetButton){
+            new AlertDialog.Builder(mActivity)
+                .setTitle(R.string.clear_all_pref)
+                .setMessage(R.string.reset_all_pref_warning)
+                .setPositiveButton(R.string.ok_text, new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface arg0, int arg1){
+                        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(mActivity);
+                        SharedPreferences.Editor editor = settings.edit();
+                        editor.clear();
+                        editor.commit();
+                        mActivity.finish();
+                    }
+                })
+                .setNegativeButton(R.string.cancel_text, null)
+                .show();
+        }
+        if(v == donateButton){
+            View alertView = View.inflate(mActivity, R.layout.link_alert, null);
+            TextView textView = (TextView)alertView.findViewById(R.id.link_alert_message);
+            textView.setText(Html.fromHtml(getString(R.string.donate_summary)));
+            textView.setMovementMethod(LinkMovementMethod.getInstance());
+            new AlertDialog.Builder(mActivity)
+                .setView(alertView)
+                .setTitle(R.string.donate_text)
+                .setPositiveButton(getString(R.string.buy_pro_text), new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface arg0, int arg1){
+                        Intent myIntent = new Intent();
+                        myIntent.setAction(Intent.ACTION_VIEW);
+                        myIntent.addCategory(Intent.CATEGORY_BROWSABLE);
+                        myIntent.setData(Uri.parse(getString(R.string.anymemo_pro_link)));
+                        startActivity(myIntent);
+                    }
+                })
+                .setNegativeButton(getString(R.string.cancel_text), null)
+                .show();
+        }
+        if(v == helpButton){
+            Intent myIntent = new Intent();
+            myIntent.setAction(Intent.ACTION_VIEW);
+            myIntent.addCategory(Intent.CATEGORY_BROWSABLE);
+            myIntent.setData(Uri.parse(WEBSITE_VERSION));
+            startActivity(myIntent);
+        }
+        if(v == aboutButton){
+            View alertView = View.inflate(mActivity, R.layout.link_alert, null);
+            TextView textView = (TextView)alertView.findViewById(R.id.link_alert_message);
+            textView.setText(Html.fromHtml(getString(R.string.about_text)));
+            textView.setMovementMethod(LinkMovementMethod.getInstance());
+            new AlertDialog.Builder(mActivity)
+                .setView(alertView)
+                .setTitle(getString(R.string.about_title) + " " + getString(R.string.app_full_name) + " " + getString(R.string.app_version))
+                .setPositiveButton(getString(R.string.ok_text), null)
+                .setNegativeButton(getString(R.string.about_version), new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface arg0, int arg1){
+                        Intent myIntent = new Intent();
+                        myIntent.setAction(Intent.ACTION_VIEW);
+                        myIntent.addCategory(Intent.CATEGORY_BROWSABLE);
+                        myIntent.setData(Uri.parse(WEBSITE_VERSION));
+                        startActivity(myIntent);
+                    }
+                })
+                .show();
+        }
+
+    }
+
+}
