c47c0d595b74bd37ea05692c163531ec2163da8c
Howard Liberty <liberty@anymemo.org>
2012-03-04 16:42:55 -0800

    Modify ORMLite to support Android 1.6.

---
+++ src/com/j256/ormlite/dao/CloseableWrappedIterableImpl.java
@@ -0,0 +1,42 @@
+package com.j256.ormlite.dao;
+
+import java.sql.SQLException;
+
+/**
+ * Class which is used to help folks use for loops but still close at the end. This is a wrapper to allow multiple
+ * threads to iterate across the same dao or the same lazy collection at the same time. See
+ * {@link Dao#getWrappedIterable()} or {@link ForeignCollection#getWrappedIterable()}.
+ * 
+ * @author graywatson
+ */
+public class CloseableWrappedIterableImpl<T> implements CloseableWrappedIterable<T> {
+
+	private final CloseableIterable<T> iterable;
+	private CloseableIterator<T> iterator;
+
+	public CloseableWrappedIterableImpl(CloseableIterable<T> iterable) {
+		this.iterable = iterable;
+	}
+
+	public CloseableIterator<T> iterator() {
+		return closeableIterator();
+	}
+
+	public CloseableIterator<T> closeableIterator() {
+		try {
+			// close an existing iterator, if any
+			close();
+		} catch (SQLException e) {
+			// ignored
+		}
+		iterator = iterable.closeableIterator();
+		return iterator;
+	}
+
+	public void close() throws SQLException {
+		if (iterator != null) {
+			iterator.close();
+			iterator = null;
+		}
+	}
+}
