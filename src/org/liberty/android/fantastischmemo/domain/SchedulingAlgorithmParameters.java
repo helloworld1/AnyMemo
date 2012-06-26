9ddceaedd2752658dda139bf9caadccb486cf6c6
Howard Liberty <liberty@anymemo.org>
2012-06-25 17:37:03 -0700

    Complete SchedulingAlgorithmParameters to retrieve the settings.

--- src/org/liberty/android/fantastischmemo/domain/SchedulingAlgorithmParameters.java
+++ src/org/liberty/android/fantastischmemo/domain/SchedulingAlgorithmParameters.java
@@ -7,8 +7,23 @@ import android.preference.PreferenceManager;
 
 public class SchedulingAlgorithmParameters {
 
-    SharedPreferences settings;
-    SharedPreferences.Editor editor;
+    private SharedPreferences settings;
+
+    private SharedPreferences.Editor editor;
+
+    private static float[] defaultInitialIntervals = {0.0f, 0.0f, 1.0f, 3.0f, 4.0f, 5.0f};
+
+    private static float defaultMinimalInterval = 0.9f;
+
+    private static float[] defaultFailedGradingIntervals = {0.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f};
+
+    private static float defaultInitialEasiness = 2.5f;
+
+    private static float defaultMinimalEasiness = 1.3f;
+
+    private static float[] defaultEasinessIncrementals = {0.0f, 0.0f, -0.16f, -0.14f, 0.0f, 0.10f};
+
+    private static boolean defaultEnableNoise = true;
 
     public SchedulingAlgorithmParameters(Context context) {
     	settings = PreferenceManager.getDefaultSharedPreferences(context);
@@ -16,7 +31,34 @@ public class SchedulingAlgorithmParameters {
     }
 
     public float getInitialInterval(int grade) {
-    	// TODO: Shouldn't be 0.0 for default value
-        return settings.getFloat("initial_grading_interval_" + grade, 0.0f);
+        assert grade < defaultInitialIntervals.length;
+        return settings.getFloat("initial_grading_interval_" + grade, defaultInitialIntervals[grade]);
     }
+
+    public float getMinimalInterval() {
+        return settings.getFloat("minimal_interval", defaultMinimalInterval);
+    }
+
+    public float getFailedGradingInterval(int grade) {
+        assert grade < defaultFailedGradingIntervals.length;
+        return settings.getFloat("failed_grading_interval_" + grade, defaultFailedGradingIntervals[grade]);
+    }
+
+    public float getInitialEasiness() {
+        return settings.getFloat("initial_easiness", defaultInitialEasiness);
+    }
+
+    public float getMinimalEasiness() {
+        return settings.getFloat("minimal_easiness", defaultMinimalEasiness);
+    }
+
+    public float getEasinessIncremental(int grade) {
+        assert grade < defaultFailedGradingIntervals.length;
+        return settings.getFloat("easiness_incremental_" + grade, defaultEasinessIncrementals[grade]);
+    }
+
+    public boolean getEnableNoise() {
+        return settings.getBoolean("enable_noise", defaultEnableNoise);
+    }
+
 }
