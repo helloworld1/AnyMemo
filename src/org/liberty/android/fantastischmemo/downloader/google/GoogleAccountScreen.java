d3af6c51c04399c585b65102274b44fb6e53823d
Howard Liberty <liberty@anymemo.org>
2012-07-09 12:30:29 -0700

    experiments 1

--- src/org/liberty/android/fantastischmemo/downloader/google/GoogleAccountScreen.java
+++ src/org/liberty/android/fantastischmemo/downloader/google/GoogleAccountScreen.java
@@ -1,13 +1,45 @@
 package org.liberty.android.fantastischmemo.downloader.google;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
+
 import org.apache.mycommons.io.IOUtils;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+
+import com.google.api.client.googleapis.services.GoogleKeyInitializer;
+
+import com.google.api.client.http.HttpTransport;
+
+import com.google.api.client.http.apache.ApacheHttpTransport;
+
+import com.google.api.client.http.json.JsonHttpRequest;
+import com.google.api.client.http.json.JsonHttpRequestInitializer;
+
+import com.google.api.client.json.JsonFactory;
+
+import com.google.api.client.json.gson.GsonFactory;
+
+import com.google.api.services.drive.Drive;
+
+import com.google.api.services.drive.Drive.Files;
+
+import com.google.api.services.drive.DriveRequest;
+
+import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.model.FileList;
 
 import android.accounts.Account;
 import android.accounts.AccountManager;
@@ -19,13 +51,16 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+
+import android.util.Log;
 import android.widget.Toast;
 
 public class GoogleAccountScreen extends Activity {
     /** Called when the activity is first created. */
     private static final int DIALOG_ACCOUNTS = 0;
     private static final int REQUEST_AUTHENTICATE = 0;
-    protected static final String AUTH_TOKEN_TYPE = "ah";
+    private static final String TAG = "GoogleAccountScreen";
+    protected static final String AUTH_TOKEN_TYPE = "oauth2:https://www.googleapis.com/auth/drive";
     private String authToken;
     Context context;    
 
@@ -147,30 +182,83 @@ public class GoogleAccountScreen extends Activity {
         }
             }
 
-    private void authenticatedClientLogin(String authToken) {
+    private void authenticatedClientLogin(final String authToken) {
         this.authToken = authToken;
         Toast.makeText(this,"Token "+authToken, Toast.LENGTH_LONG).show();
-        String apiKey = "AIzaSyBgSh_sLf2FUvKgc6ZjoYXLvf6viYWFZSo";
-
+        final String apiKey = "AIzaSyBgSh_sLf2FUvKgc6ZjoYXLvf6viYWFZSo";
         try {
-            URL url = new URL("https://docs.google.com/feeds/default/private/full?v=3");
-            URLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.addRequestProperty("client_id", "45533559525.apps.googleusercontent.com");
-            conn.addRequestProperty("client_secret", "74rz27lrTr9mWNnipgxXwtEd");
-            conn.setRequestProperty("Authorization", "OAuth " + authToken);
-            InputStream in = new BufferedInputStream(conn.getInputStream());
-            try {
-                String s = IOUtils.toString(in);
-                System.out.println("SSSSSSSSSSSS: " + s);
-            }
-            finally {
-                in.close();
-            }
+        URL url = new URL("https://www.googleapis.com/drive/v2/files?key=" + apiKey);
+        HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+        conn.addRequestProperty("client_id", "45533559525-77qfkgj6skoo222jqu2r18p2gcgreouc.apps.googleusercontent.com");
+        conn.addRequestProperty("client_secret", "oD_rScGrwBO0iO3lArIktDoL");
+        conn.addRequestProperty("Authorization", "OAuth " + authToken);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(
+                                    conn.getInputStream()));
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            System.out.println(inputLine);
         }
-            catch (Exception e) {
-                throw new RuntimeException(e);
-            }
+        in.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        //final HttpTransport transport = new ApacheHttpTransport(ApacheHttpTransport.newDefaultHttpClient());
+
+        //final JsonFactory jsonFactory = new GsonFactory();
+        //GoogleCredential credential = new GoogleCredential();
+        //credential.setAccessToken(authToken);
+
+        //Drive service = new Drive.Builder(transport, jsonFactory, credential)
+        //    .setJsonHttpRequestInitializer(new JsonHttpRequestInitializer() {
+
+        //        @Override
+        //        public void initialize(JsonHttpRequest request) throws IOException {
+        //            DriveRequest driveRequest = (DriveRequest) request;
+        //            driveRequest.setPrettyPrint(true);
+        //            driveRequest.setKey(apiKey);
+        //            driveRequest.setOauthToken(authToken);
+        //        }
+        //    })
+
+        //    .build();
+
+        //List<File> files = retrieveAllFiles(service);
+        //Log.i("CHEOK", "size is " + files.size());
+        //for (File file : files) {
+        //    Log.i(TAG, "title = " + file.getTitle());
+        //}
+
         //((GoogleHeaders) transport.defaultHeaders).setGoogleLogin(authToken);
         //authenticated();
     }
+
+private static List<File> retrieveAllFiles(Drive service) {
+        List<File> result = new ArrayList<File>();
+        Files.List request = null;
+        try {
+            request = service.files().list();
+        } catch (IOException e) {
+            Log.e(TAG, "", e);
+            return result;
+        }
+
+        do {
+            try {
+                FileList files = request.execute();
+                for (File f : files.getItems()) {
+
+                result.addAll(files.getItems());
+                request.setPageToken(files.getNextPageToken());
+                }
+            } catch (IOException e) {
+                Log.e(TAG, "", e);
+                request.setPageToken(null);
+            }
+        } while (request.getPageToken() != null && request.getPageToken().length() > 0);
+
+        Log.i("CHEOK", "yup!");
+        return result;
+    } 
+
 }
