856cd727cbc4dd315c4a9baf2335b96f614a0218
Howard Liberty <liberty@anymemo.org>
2012-08-04 11:35:04 -0700

    Google drive can import learning data now.

--- src/org/liberty/android/fantastischmemo/downloader/google/GoogleDriveUploadHelper.java
+++ src/org/liberty/android/fantastischmemo/downloader/google/GoogleDriveUploadHelper.java
@@ -19,6 +19,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 package org.liberty.android.fantastischmemo.downloader.google;
 
+import java.text.SimpleDateFormat;
+
 import java.util.List;
 
 import java.util.concurrent.Callable;
@@ -31,6 +33,7 @@ import org.liberty.android.fantastischmemo.dao.CategoryDao;
 import org.liberty.android.fantastischmemo.dao.LearningDataDao;
 
 import org.liberty.android.fantastischmemo.domain.Card;
+import org.liberty.android.fantastischmemo.domain.LearningData;
 
 import org.liberty.android.fantastischmemo.downloader.google.Cells;
 
@@ -40,6 +43,8 @@ public class GoogleDriveUploadHelper {
     private Context mContext;
 
     private final String authToken;
+
+    private static SimpleDateFormat ISO8601_FORMATTER = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
 
     public GoogleDriveUploadHelper(Context context, String authToken) {
         this.authToken = authToken;
@@ -105,8 +110,39 @@ public class GoogleDriveUploadHelper {
             cells.addCell(i + 2, 4, c.getNote());
         }
 
-        // Insert rows into worksheet
+        // upload card's rows into worksheet
         CellsFactory.uploadCells(newSpreadsheet, cardsWorksheet, cells, authToken);
+
+        // Now deal with learning data
+        Worksheet learningDataWorksheet =
+            WorksheetFactory.createWorksheet(newSpreadsheet, "learning_data", cardList.size() + 1, 9, authToken);
+        Cells learningDataCells = new Cells();
+
+        // The first row is the header.
+        learningDataCells.addCell(1, 1, "acqReps");
+        learningDataCells.addCell(1, 2, "acqRepsSinceLapse");
+        learningDataCells.addCell(1, 3, "easiness");
+        learningDataCells.addCell(1, 4, "grade");
+        learningDataCells.addCell(1, 5, "lapses");
+        learningDataCells.addCell(1, 6, "lastLearnDate");
+        learningDataCells.addCell(1, 7, "nextLearnDate");
+        learningDataCells.addCell(1, 8, "retReps");
+        learningDataCells.addCell(1, 9, "retRepsSinceLapse");
+        for (int i = 0; i < cardList.size(); i++) {
+            LearningData ld = cardList.get(i).getLearningData();
+            learningDataCells.addCell(i + 2, 1, Integer.toString(ld.getAcqReps()));
+            learningDataCells.addCell(i + 2, 2, Integer.toString(ld.getAcqRepsSinceLapse()));
+            learningDataCells.addCell(i + 2, 3, Float.toString(ld.getEasiness()));
+            learningDataCells.addCell(i + 2, 4, Integer.toString(ld.getGrade()));
+            learningDataCells.addCell(i + 2, 5, Integer.toString(ld.getLapses()));
+            learningDataCells.addCell(i + 2, 6, ISO8601_FORMATTER.format(ld.getLastLearnDate()));
+            learningDataCells.addCell(i + 2, 7, ISO8601_FORMATTER.format(ld.getNextLearnDate()));
+            learningDataCells.addCell(i + 2, 8, Integer.toString(ld.getRetReps()));
+            learningDataCells.addCell(i + 2, 9, Integer.toString(ld.getRetRepsSinceLapse()));
+        }
+
+        // upload learning data rows into worksheet
+        CellsFactory.uploadCells(newSpreadsheet, learningDataWorksheet, learningDataCells, authToken);
 
 
         // Put new spreadsheet into the folder
