e80e1c1995d611bfc1013904538ee21735830363
Xiaoyu Shi <xiaoyushi@Xiaoyus-MacBook-Air.local>
2012-08-18 13:10:17 -0700

    Add remove useless string value script.

---
+++ removeUnusedStrings.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+srcDir='src' 
+resDir='res'
+stringsFile='res/values/strings.xml'
+
+deleteLine ()
+{
+        # sed -i is not compatibal with BSD and GNU
+        # mktemp not compatibal
+        
+        files=`ls -R $resDir/values*/strings.xml`
+        for file in $files
+        do
+                sed /$1/d $file > /tmp/tmp.file
+                mv /tmp/tmp.file $file
+        done
+}
+
+stringNames=`grep -o 'name=\".*\"\s*>' $stringsFile | sed 's/name=\"\(.*\)\"\s*>/\1/g'`
+
+for name in $stringNames
+do
+        apperanceCount=`grep -r --include="*.java" --include="*.xml" --exclude="strings.xml" $name $srcDir $resDir | wc -l`
+        if [ "$apperanceCount" -eq 0 ]
+        then
+                echo "deleting string: $name" 
+                deleteLine $name
+        fi
+done
