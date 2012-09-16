8ad21587f1d1ed846f4ac08d8a38d679007b2089
shixiaoyu <shixiaoyu.zju@gmail.com>
2012-09-15 23:11:03 -0700

    Added basic auto speak place holder.

--- src/org/liberty/android/fantastischmemo/ui/AutoSpeakFragment.java
+++ src/org/liberty/android/fantastischmemo/ui/AutoSpeakFragment.java
@@ -8,18 +8,45 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 
 public class AutoSpeakFragment extends Fragment {
-	
-	@Override
-	public View onCreateView(LayoutInflater inflater,
+    
+    private static final String TAG = "AutoSpeakFragment";
+    private ImageButton playButton;
+    private boolean isPlaying = false;
+    
+    private View.OnClickListener buttonListener = new View.OnClickListener() {
+        
+        @Override
+        public void onClick(View v) {
+            isPlaying = !isPlaying;
+            
+            if(isPlaying) {
+                playButton.setSelected(true);
+            } else {
+                playButton.setSelected(false);
+            }
+        }
+    };
+    
+    @Override
+    public View onCreateView(LayoutInflater inflater,
             ViewGroup container,
             Bundle savedInstanceState) {
-		View v = inflater.inflate(R.layout.auto_speak_layout, container, false);
-		
-		
-		return v;
-	}
-	
+        View v = inflater.inflate(R.layout.auto_speak_layout, container, false);
+        playButton = (ImageButton) v.findViewById(R.id.auto_speak_play_button);
+        playButton.setOnClickListener(buttonListener);
+        
+        return v;
+    }
+    
+    
+    
+    
+    public void onClick(View v) {
+        
+    }
+    
 }
