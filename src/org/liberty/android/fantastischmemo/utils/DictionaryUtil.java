c03ba4701f0fc7d2c2840c9a68c8a098ac74e56e
Howard Liberty <liberty@anymemo.org>
2012-10-15 15:30:58 -0700

    Enhanced Dictionary look up function so all words can be looked up.

--- src/org/liberty/android/fantastischmemo/utils/DictionaryUtil.java
+++ src/org/liberty/android/fantastischmemo/utils/DictionaryUtil.java
@@ -19,16 +19,19 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 package org.liberty.android.fantastischmemo.utils;
 
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 import org.liberty.android.fantastischmemo.R;
 
 import org.liberty.android.fantastischmemo.domain.Option;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 
-import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
-
-import android.util.Log;
 
 import android.view.Gravity;
 
@@ -40,6 +43,47 @@ public class DictionaryUtil {
     public DictionaryUtil(Activity activity) {
         mActivity = activity;
         option = new Option(mActivity);
+    }
+
+    /*
+     * Show a dialog to look up words in the text
+     */
+    public void showLookupListDialog(String text) {
+
+		// Replace break
+		String processed_str = text.replaceAll("\\<br\\>", " " );
+		// Remove HTML
+		processed_str = processed_str.replaceAll("\\<.*?>", " ");
+		// Remove () [] and their content
+		processed_str = processed_str.replaceAll("\\[.*?\\]", " ");
+        // Remove the XML special character
+		processed_str = processed_str.replaceAll("\\[.*?\\]", " ");
+
+        String[] words = processed_str.split("[\\s\t\n\r\f,;.(){}?!]+");
+
+        System.out.println("Splitted : " + Arrays.toString(words));
+
+        // Maintina the order of words in the original text
+        Set<String> wordSet = new LinkedHashSet<String>();
+
+        for (String word : words) {
+            wordSet.add(word);
+        }
+
+        final String[] wordsToDisplay = new String[wordSet.size()];
+        wordSet.toArray(wordsToDisplay);
+
+        new AlertDialog.Builder(mActivity)
+            .setTitle(R.string.look_up_text)
+            .setItems(wordsToDisplay, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                    lookupDictionary(wordsToDisplay[which]);
+                }
+
+            })
+            .show();
     }
 
     public void lookupDictionary(String lookupWord) {
