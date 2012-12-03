db9ad6b98acb84c3337ca2a56ddd6cc993009e4d
Howard Liberty <liberty@anymemo.org>
2012-12-03 15:40:28 -0800

    Fix the StudyActivityFunctionTest.

--- test/src/org/liberty/android/fantastischmemo/test/ui/StudyActivityFunctionTest.java
+++ test/src/org/liberty/android/fantastischmemo/test/ui/StudyActivityFunctionTest.java
@@ -3,22 +3,26 @@ package org.liberty.android.fantastischmemo.test.ui;
 import org.liberty.android.fantastischmemo.AnyMemoDBOpenHelper;
 import org.liberty.android.fantastischmemo.AnyMemoDBOpenHelperManager;
 import org.liberty.android.fantastischmemo.R;
-
 import org.liberty.android.fantastischmemo.dao.CardDao;
 import org.liberty.android.fantastischmemo.dao.LearningDataDao;
+import org.liberty.android.fantastischmemo.ui.StudyActivity;
 
-import org.liberty.android.fantastischmemo.ui.AnyMemo;
+import android.content.Intent;
+import android.test.ActivityInstrumentationTestCase2;
+import android.view.View;
 
 import com.jayway.android.robotium.solo.Solo;
 
-import android.test.ActivityInstrumentationTestCase2;
+public class StudyActivityFunctionTest extends ActivityInstrumentationTestCase2<StudyActivity> {
 
-public class StudyActivityFunctionTest extends ActivityInstrumentationTestCase2<AnyMemo> {
+    protected StudyActivity mActivity;
 
-    protected AnyMemo mActivity;
+    private View questionView;
+
+    private View answerView;
 
     public StudyActivityFunctionTest() {
-        super("org.liberty.android.fantastischmemo", AnyMemo.class);
+        super("org.liberty.android.fantastischmemo", StudyActivity.class);
     }
 
     private Solo solo;
@@ -26,19 +30,20 @@ public class StudyActivityFunctionTest extends ActivityInstrumentationTestCase2<
     public void setUp() throws Exception{
         UITestHelper uiTestHelper = new UITestHelper(getInstrumentation());
         uiTestHelper.clearPreferences();
+        uiTestHelper.setUpFBPDatabase();
         
-        mActivity = this.getActivity();
-        solo = new Solo(getInstrumentation(), mActivity);
-        solo.sleep(1000);
+        Intent intent = new Intent();
+        intent.putExtra(StudyActivity.EXTRA_DBPATH, UITestHelper.SAMPLE_DB_PATH);
+        setActivityIntent(intent);
 
-        if (solo.searchText("New version")) {
-            solo.clickOnText(solo.getString(R.string.ok_text));
-        }
-        solo.sleep(4000);
-        solo.clickLongOnText(UITestHelper.SAMPLE_DB_NAME);
-        solo.clickOnText(solo.getString(R.string.study_text));
-        solo.waitForActivity("StudyActivity");
-        solo.sleep(4000);
+        mActivity = this.getActivity();
+
+        questionView = mActivity.findViewById(R.id.question);
+
+        answerView = mActivity.findViewById(R.id.answer);
+
+        solo = new Solo(getInstrumentation(), mActivity);
+        solo.sleep(2000);
     }
 
 
@@ -46,11 +51,11 @@ public class StudyActivityFunctionTest extends ActivityInstrumentationTestCase2<
 
 
         // Success 1st card
-        solo.clickOnText("Show answer");
+        solo.clickOnView(answerView);
         solo.clickOnText(solo.getString(R.string.memo_btn2_text));
 
         // Success 2nd card
-        solo.clickOnText("Show answer");
+        solo.clickOnView(answerView);
         solo.clickOnText(solo.getString(R.string.memo_btn2_text));
 
         // Undo
@@ -64,10 +69,9 @@ public class StudyActivityFunctionTest extends ActivityInstrumentationTestCase2<
         assertTrue(solo.searchText("hair"));
 
         // Fail 2nd card 
-        solo.clickOnText("Show answer");
+        solo.clickOnView(answerView);
         solo.clickOnText(solo.getString(R.string.memo_btn0_text));
 
-        solo.goBack();
         solo.goBack();
         solo.sleep(5000);
 
@@ -85,10 +89,8 @@ public class StudyActivityFunctionTest extends ActivityInstrumentationTestCase2<
     }
 
     public void testDeleteCard() throws Exception {
-        solo.clickLongOnText("head");
-        solo.clickOnText(solo.getString(R.string.delete_text));
+        solo.clickOnMenuItem(solo.getString(R.string.delete_text));
         solo.clickOnText(solo.getString(R.string.ok_text));
-        solo.sleep(3000);
         solo.goBack();
         solo.sleep(3000);
         // asssert db state
@@ -106,7 +108,7 @@ public class StudyActivityFunctionTest extends ActivityInstrumentationTestCase2<
     }
 
     public void testSkipCard() throws Exception {
-        solo.clickLongOnText("head");
+        solo.clickOnMenuItem(solo.getString(R.string.skip_text));
         // press skip
         solo.clickOnText(solo.getString(R.string.skip_text));
         solo.clickOnText(solo.getString(R.string.ok_text));
@@ -130,21 +132,19 @@ public class StudyActivityFunctionTest extends ActivityInstrumentationTestCase2<
     }
 
     public void testGotoPreviewScreen() {
-        solo.clickLongOnText("head");
         // press skip
-        solo.clickOnText(solo.getString(R.string.goto_prev_screen));
+        solo.clickOnMenuItem(solo.getString(R.string.goto_prev_screen));
         assertTrue(solo.waitForActivity("PreviewEditActivity"));
     }
 
     public void testGotoDetailScreen() {
-        solo.sendKey(Solo.MENU);
-        solo.clickOnText(solo.getString(R.string.detail_menu_text));
+        // The way to click menu item in action bar
+        getInstrumentation().invokeMenuActionSync(mActivity, R.id.menudetail, 0);
         assertTrue(solo.waitForActivity("DetailScreen"));
     }
 
     public void testGotoSettingsScreen() {
-        solo.sendKey(Solo.MENU);
-        solo.clickOnText(solo.getString(R.string.settings_menu_text));
+        solo.clickOnMenuItem(solo.getString(R.string.settings_menu_text));
         assertTrue(solo.waitForActivity("SettingsScreen"));
     }
 
