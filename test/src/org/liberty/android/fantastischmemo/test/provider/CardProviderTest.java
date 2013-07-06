3345c1c6d10952f59c44a1d4f82f29311dc802c6
Howard Liberty <liberty@anymemo.org>
2013-07-06 13:10:21 -0700

    Fix a typo in the test.

--- test/src/org/liberty/android/fantastischmemo/test/provider/CardProviderTest.java
+++ test/src/org/liberty/android/fantastischmemo/test/provider/CardProviderTest.java
@@ -10,19 +10,19 @@ import android.test.suitebuilder.annotation.SmallTest;
 
 public class CardProviderTest extends AbstractExistingDBTest {
 
-    private static String AUTHROITY = "org.liberty.android.fantastischmemo.cardprovider";
+    private static String AUTHORITY = "org.liberty.android.fantastischmemo.cardprovider";
 
     @SmallTest
     public void testNonExistingDbShouldReturnNullCursor() {
         ContentResolver cr = getContext().getContentResolver();
-        Cursor cursor = cr.query(Uri.parse("content://" + AUTHROITY + "/" + "No_such_db_exists.db"+ "/count"),null, null, null, null);
+        Cursor cursor = cr.query(Uri.parse("content://" + AUTHORITY + "/" + "No_such_db_exists.db"+ "/count"),null, null, null, null);
         assertNull(cursor);
     }
 
     @SmallTest
     public void testCardCount() {
         ContentResolver cr = getContext().getContentResolver();
-        Cursor cursor = cr.query(Uri.parse("content://" + AUTHROITY + "/" + TestHelper.SAMPLE_DB_NAME + "/count"),null, null, null, null);
+        Cursor cursor = cr.query(Uri.parse("content://" + AUTHORITY + "/" + TestHelper.SAMPLE_DB_NAME + "/count"),null, null, null, null);
         assertEquals(1, cursor.getCount());
         assertTrue(cursor.moveToFirst());
         assertEquals(28, cursor.getInt(0));
@@ -31,14 +31,14 @@ public class CardProviderTest extends AbstractExistingDBTest {
     @SmallTest
     public void testGetRandomCards() {
         ContentResolver cr = getContext().getContentResolver();
-        Cursor cursor = cr.query(Uri.parse("content://" + AUTHROITY + "/" + TestHelper.SAMPLE_DB_NAME + "/random/10"),null, null, null, null);
+        Cursor cursor = cr.query(Uri.parse("content://" + AUTHORITY + "/" + TestHelper.SAMPLE_DB_NAME + "/random/10"),null, null, null, null);
         assertEquals(10, cursor.getCount());
     }
 
     @SmallTest
     public void testGetByOrdinal() {
         ContentResolver cr = getContext().getContentResolver();
-        Cursor cursor = cr.query(Uri.parse("content://" + AUTHROITY + "/" + TestHelper.SAMPLE_DB_NAME + "/ordinal/3"),null, null, null, null);
+        Cursor cursor = cr.query(Uri.parse("content://" + AUTHORITY + "/" + TestHelper.SAMPLE_DB_NAME + "/ordinal/3"),null, null, null, null);
         assertEquals(1, cursor.getCount());
         assertTrue(cursor.moveToFirst());
         assertEquals(3, cursor.getInt(cursor.getColumnIndex("ordinal")));
@@ -49,7 +49,7 @@ public class CardProviderTest extends AbstractExistingDBTest {
     @SmallTest
     public void testGetById() {
         ContentResolver cr = getContext().getContentResolver();
-        Cursor cursor = cr.query(Uri.parse("content://" + AUTHROITY + "/" + TestHelper.SAMPLE_DB_NAME + "/id/3"),null, null, null, null);
+        Cursor cursor = cr.query(Uri.parse("content://" + AUTHORITY + "/" + TestHelper.SAMPLE_DB_NAME + "/id/3"),null, null, null, null);
         assertEquals(1, cursor.getCount());
         assertTrue(cursor.moveToFirst());
         assertEquals(3, cursor.getInt(cursor.getColumnIndex("ordinal")));
@@ -60,7 +60,7 @@ public class CardProviderTest extends AbstractExistingDBTest {
     @SmallTest
     public void testGetCardWithStartOrdinalAndCount() {
         ContentResolver cr = getContext().getContentResolver();
-        Cursor cursor = cr.query(Uri.parse("content://" + AUTHROITY + "/" + TestHelper.SAMPLE_DB_NAME + "/start_ordinal/3/count/5"),null, null, null, null);
+        Cursor cursor = cr.query(Uri.parse("content://" + AUTHORITY + "/" + TestHelper.SAMPLE_DB_NAME + "/start_ordinal/3/count/5"),null, null, null, null);
         assertEquals(5, cursor.getCount());
         assertTrue(cursor.moveToFirst());
         assertEquals(3, cursor.getInt(cursor.getColumnIndex("ordinal")));
@@ -73,7 +73,7 @@ public class CardProviderTest extends AbstractExistingDBTest {
     @SmallTest
     public void testGetAllCards() {
         ContentResolver cr = getContext().getContentResolver();
-        Cursor cursor = cr.query(Uri.parse("content://" + AUTHROITY + "/" + TestHelper.SAMPLE_DB_NAME + "/all"),null, null, null, null);
+        Cursor cursor = cr.query(Uri.parse("content://" + AUTHORITY + "/" + TestHelper.SAMPLE_DB_NAME + "/all"),null, null, null, null);
         assertEquals(28, cursor.getCount());
         assertTrue(cursor.moveToFirst());
         assertEquals(1, cursor.getInt(cursor.getColumnIndex("ordinal")));
