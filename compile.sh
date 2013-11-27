3cabb80603fc8aecf3bdef2e90e96f59f8834e8e
Howard Liberty <liberty@anymemo.org>
2013-11-27 15:50:51 -0800

    Tests are using gradle now. Updated to use 0.6+ plugin.

--- compile.sh
+++ compile.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 trap exit ERR
 ant debug install
-adb shell am start -n org.liberty.android.fantastischmemo/org.liberty.android.fantastischmemo.ui.AnyMemo
--- test/compile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-trap exit ERR
-ant debug install
