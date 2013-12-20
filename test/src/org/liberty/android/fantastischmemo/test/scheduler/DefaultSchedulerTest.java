8a3b7852314014001bbdab73db5fa50b56353e26
Howard Liberty <liberty@anymemo.org>
2013-12-19 16:43:27 -0800

    Add file for DefaultSchedulerTest.

---
+++ test/src/org/liberty/android/fantastischmemo/test/scheduler/DefaultSchedulerTest.java
@@ -0,0 +1,9 @@
+package org.liberty.android.fantastischmemo.test.scheduler;
+
+import org.liberty.android.fantastischmemo.test.AbstractPreferencesTest;
+
+public class DefaultSchedulerTest extends AbstractPreferencesTest {
+
+}
+
+
