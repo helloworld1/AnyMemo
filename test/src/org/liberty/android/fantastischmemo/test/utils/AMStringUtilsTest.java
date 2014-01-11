592e1d72923c70fa12b221c9cc297d5de7c57bfc
Howard Liberty <liberty@anymemo.org>
2014-01-11 10:42:35 -0800

    Rename a misnamed test.

--- test/src/org/liberty/android/fantastischmemo/test/utils/AMStringUtilsTest.java
+++ test/src/org/liberty/android/fantastischmemo/test/utils/AMStringUtilsTest.java
@@ -10,16 +10,6 @@ import android.test.suitebuilder.annotation.SmallTest;
 public class AMStringUtilsTest extends AndroidTestCase {
 
     @SmallTest
-    public void testConvertDayIntervalToDisplayString() {
-        AMDateUtil amDateUtil = new AMDateUtil(getContext());
-        assertEquals("5.0 " + getContext().getString(R.string.day_text), amDateUtil.convertDayIntervalToDisplayString(5.00));
-        assertEquals("3.2 " + getContext().getString(R.string.day_text), amDateUtil.convertDayIntervalToDisplayString(3.22));
-        assertEquals("3.3 " + getContext().getString(R.string.day_text), amDateUtil.convertDayIntervalToDisplayString(3.26));
-        assertEquals("1.8 " + getContext().getString(R.string.week_text), amDateUtil.convertDayIntervalToDisplayString(12.345));
-        assertEquals("1.6 " + getContext().getString(R.string.year_text), amDateUtil.convertDayIntervalToDisplayString(594.322));
-    }
-
-    @SmallTest
     public void testStripHTMLWithNoHtml() {
         String noHtmlText = "I do not have html";
         assertEquals("I do not have html", AMStringUtils.stripHTML(noHtmlText));
