08f97011371589c9d7ca9dab53c061f2ab269629
Howard Liberty <liberty@anymemo.org>
2014-01-24 17:00:34 -0800

    sign-apk.sh accept flavor as an argument.

--- scripts/sign-apk.sh
+++ scripts/sign-apk.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-jarsigner -verbose -digestalg SHA1 -sigalg MD5withRSA -keystore ~/Documents/keys/liberty-android-release.keystore build/apk/AnyMemo-release-unsigned.apk liberty-android-key
-zipalign -v 4 build/apk/AnyMemo-release-unsigned.apk build/apk/AnyMemo-release.apk
+flavor=$1
+if [[ -z "$flavor" ]]; then
+	echo "Usage: $0 flavor"
+	echo "Flavor can be either free and pro."
+	exit -1
+fi
+jarsigner -verbose -digestalg SHA1 -sigalg MD5withRSA -keystore ~/Documents/keys/liberty-android-release.keystore "build/apk/AnyMemo-$flavor-release-unsigned.apk" liberty-android-key
+zipalign -v 4 "build/apk/AnyMemo-$flavor-release-unsigned.apk" "build/apk/AnyMemo-$flavor-release.apk"
