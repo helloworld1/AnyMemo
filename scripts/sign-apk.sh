65b234833888297da17af2b5a8123cc2cb0be45b
Howard Liberty <liberty@anymemo.org>
2014-06-07 13:06:48 -0700

    Update apk signing script

--- scripts/sign-apk.sh
+++ scripts/sign-apk.sh
@@ -6,5 +6,5 @@ if [[ -z "$flavor" ]]; then
 	echo "Flavor can be either free and pro."
 	exit -1
 fi
-jarsigner -verbose -digestalg SHA1 -sigalg MD5withRSA -keystore ~/Documents/keys/liberty-android-release.keystore "build/apk/AnyMemo-$flavor-release-unsigned.apk" liberty-android-key
-zipalign -v 4 "build/apk/AnyMemo-$flavor-release-unsigned.apk" "build/apk/AnyMemo-$flavor-release.apk"
+jarsigner -verbose -digestalg SHA1 -sigalg MD5withRSA -keystore ~/Documents/keys/liberty-android-release.keystore "build/outputs/apk/AnyMemo-$flavor-release-unsigned.apk" liberty-android-key
+zipalign -v 4 "build/outputs/apk/AnyMemo-$flavor-release-unsigned.apk" "build/outputs/apk/AnyMemo-$flavor-release.apk"
