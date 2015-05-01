681fbb8d5134d7c79e8a87240bd10031ae8e0ca7
Howard Liberty <liberty@anymemo.org>
2015-04-30 18:09:58 -0700

    Move folder structure to gradle format

--- jni/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE    := native_db_helper
-LOCAL_SRC_FILES := native_db_helper.c sqlite3.c
-LOCAL_LDLIBS := -llog
-
-include $(BUILD_SHARED_LIBRARY)
