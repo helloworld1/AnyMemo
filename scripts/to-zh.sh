88b8f917ecb1e7fd9d2af7137376287689e42bde
Howard Liberty <liberty@anymemo.org>
2015-05-26 11:36:25 -0700

    Fix inaccurate Chinese translations.

--- scripts/to-zh.sh
+++ scripts/to-zh.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-if [ -d "res/values-zh-rTW" ]
+if [ -d "src/main/res/values-zh-rTW" ]
 then
-        rm -rf res/values-zh-rTW
+        rm -rf src/main/res/values-zh-rTW
 fi
-mkdir res/values-zh-rTW
-iconv -f utf8 -t gb2312 res/values-zh-rCN/strings.xml | iconv -f gb2312 -t big5 | iconv -f big5 -t utf8 > res/values-zh-rTW/strings.xml 
+mkdir src/main/res/values-zh-rTW
+iconv -f utf8 -t gb2312 src/main/res/values-zh-rCN/strings.xml | iconv -f gb2312 -t big5 | iconv -f big5 -t utf8 > src/main/res/values-zh-rTW/strings.xml 
 
 
 
