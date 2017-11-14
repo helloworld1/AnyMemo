b13df17dcf6cf37d88c28efd78fc453335d76c0b
Howard Liberty <liberty@anymemo.org>
2017-11-13 22:50:59 -0800

    Update script for AGP 3.0

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 trap exit ERR
-./gradlew assembleFreeDebug
+./gradlew assembleDevApi16Debug
 adb install -r app/build/outputs/apk/AnyMemo-free-debug.apk
 adb shell am start -n org.liberty.android.fantastischmemo/org.liberty.android.fantastischmemo.ui.AnyMemo
