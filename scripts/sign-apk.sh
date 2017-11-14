cbd98f1ee4e3e1c07d9dbd60694e0707a3248a6e
Howard Liberty <liberty@anymemo.org>
2017-11-13 22:58:43 -0800

    Update signing script for AGP 3.0

--- scripts/sign-apk.sh
+++ scripts/sign-apk.sh
@@ -6,5 +6,5 @@ if [[ -z "$flavor" ]]; then
 	echo "Flavor can be either free and pro."
 	exit -1
 fi
-jarsigner -verbose -digestalg SHA1 -sigalg MD5withRSA -keystore ~/Documents/keys/liberty-android-release.keystore "app/build/outputs/apk/AnyMemo-$flavor-release-unsigned.apk" liberty-android-key
-zipalign -v 4 "app/build/outputs/apk/AnyMemo-$flavor-release-unsigned.apk" "app/build/outputs/apk/AnyMemo-$flavor-release.apk"
+jarsigner -verbose -digestalg SHA1 -sigalg MD5withRSA -keystore ~/Documents/keys/liberty-android-release.keystore "app/build/outputs/apk/${flavor}Api16/release/AnyMemo-$flavor-api16-release-unsigned.apk" liberty-android-key
+zipalign -v 4 "app/build/outputs/apk/${flavor}Api16/release/AnyMemo-$flavor-api16-release-unsigned.apk" "app/build/outputs/apk/AnyMemo-$flavor-release.apk"
