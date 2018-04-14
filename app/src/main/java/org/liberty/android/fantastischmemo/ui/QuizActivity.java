ef6f3f529adcd398a7be8513e98439deab024a9a
Howard Liberty <liberty@anymemo.org>
2018-04-14 13:41:10 -0700

    Fix QuizActivity crash

--- app/src/main/java/org/liberty/android/fantastischmemo/ui/QuizActivity.java
+++ app/src/main/java/org/liberty/android/fantastischmemo/ui/QuizActivity.java
@@ -25,8 +25,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
@@ -86,12 +84,6 @@ public class QuizActivity extends QACardActivity {
     private int totalQuizSize = -1;
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        activityComponents().inject(this);
-        super.onCreate(savedInstanceState, persistentState);
-    }
-
-    @Override
     public int getContentView() {
         return R.layout.qa_card_layout_study;
     }
@@ -125,6 +117,7 @@ public class QuizActivity extends QACardActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState){
+        activityComponents().inject(this);
         super.onCreate(savedInstanceState);
         Bundle extras = getIntent().getExtras();
         categoryId = extras.getInt(EXTRA_CATEGORY_ID, -1);
