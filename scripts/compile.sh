977031dfa3f1be8ee078fca0e3fe04968e402dd0
Howard Liberty <liberty@anymemo.org>
2018-09-19 08:59:03 -0700

    Upgrade compile script

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 trap exit ERR
 ./gradlew assembleDevApi16Debug
-adb install -r app/build/outputs/apk/AnyMemo-free-debug.apk
+adb install -r -t -d app/build/outputs/apk/devApi16/debug/AnyMemo-dev-api16-debug.apk
 adb shell am start -n org.liberty.android.fantastischmemo/org.liberty.android.fantastischmemo.ui.AnyMemo
