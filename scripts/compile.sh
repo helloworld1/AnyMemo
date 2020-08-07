53b9e494c58e071ab955f4f39923689b0b2c37b3
Howard Liberty <liberty@anymemo.org>
2020-08-07 16:13:03 -0700

    Bump target SDK and fix compile issues

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -2,4 +2,4 @@
 trap exit ERR
 ./gradlew assembleDevApi16Debug
 adb install -r -t -d app/build/outputs/apk/devApi16/debug/AnyMemo-dev-api16-debug.apk
-adb shell am start -n org.liberty.android.fantastischmemo/org.liberty.android.fantastischmemo.ui.AnyMemo
+adb shell am start -n org.liberty.android.fantastischmemodev/org.liberty.android.fantastischmemo.ui.AnyMemo
